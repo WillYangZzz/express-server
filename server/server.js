9d988b3c9d9f1089ee33d5c9a61f67c70c446b5b
Will <105895841+WillYangZzz@users.noreply.github.com>
2023-10-16 11:52:48 +1300

    create a route called /compliment within server.js

--- server/server.js
+++ server/server.js
@@ -9,9 +9,25 @@ const __dirname = Path.dirname(__filename)
 const server = express()
 
 export default server
+server.use(express.static(Path.join(__dirname, 'public')))
+server.use(express.urlencoded({ extended: false }))
+
+// // add a root called /compliment
+server.get('/', (req, res) => {
+  res.sendFile(Path.join(__dirname, 'compliment.html'))
+})
+
+// server.get('/compliment', (req, res) => {
+//   res.send('<h1> You are handsome! </h1>')
+// })
+
+// add a route called /compliment
+// server.get('public/compliment', (req, res) => {
+//   res.sendFile(Path.join(__dirname, 'compliment.html'))
+// })
 
 // server listens on port 3000
 
-server.listen(3000, () => {
-  console.log('server is listening on port 3000')
-})
+// server.listen(3000, () => {
+//   console.log('server is listening on port 3000')
+// })
