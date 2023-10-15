7c65822981c53c1e48684c082bd68b8c51bc5030
Will <105895841+WillYangZzz@users.noreply.github.com>
2023-10-16 11:14:42 +1300

    add listen port

--- server/server.js
+++ server/server.js
@@ -3,10 +3,15 @@ import * as URL from 'node:url'
 
 import express from 'express'
 
-
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
 const server = express()
 
 export default server
+
+// server listens on port 3000
+
+server.listen(3000, () => {
+  console.log('server is listening on port 3000')
+})
