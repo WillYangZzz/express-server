c3e6d49bbb88c4048c311e02236e3ec8f974aca9
Vitor <142807308+vitor-fernandess@users.noreply.github.com>
2023-10-16 12:26:12 +1300

    Added profile with query selector

--- server/server.js
+++ server/server.js
@@ -7,12 +7,20 @@ const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
 const server = express()
-server.get('/compliment', (reg, res) => {
+server.get('/compliment', (req, res) => {
   res.send('<h1>Damnnnn shortyyy!!</h1>')
 })
 
-server.get('/profile', (reg, res) => {
-  res.sendFile(Path.join(__dirname, 'silvia.html'))
+server.get('/profile/:name', (req, res) => {
+  const value = req.params.name
+  // const name = req.query.name
+  if (value === 'silvia') {
+    res.sendFile(Path.join(__dirname, `silvia.html`))
+    console.log('silvia.html')
+  } else if (value === 'sampson') {
+    res.sendFile(Path.join(__dirname, `sampson.html`))
+    console.log('Sampson.html')
+  }
 })
 
 export default server
