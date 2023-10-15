5429dec5e3f3c102e2b1bf3774609918ccb9a277
Pathik <142643540+pathik-modi@users.noreply.github.com>
2023-10-16 12:33:57 +1300

    que 4 done based on url parameter

--- server/server.js
+++ server/server.js
@@ -11,16 +11,29 @@ server.get('/compliment', (req, res) => {
   res.send('<h1>Damnnnn shortyyy!!</h1>')
 })
 
-server.get('/profile/:name', (req, res) => {
-  const value = req.params.name
+// // 3. Respond based on the query
+// server.get('/profile/:name', (req, res) => {
+//   const value = req.params.name
+//   // const name = req.query.name
+//   if (value === 'silvia') {
+//     res.sendFile(Path.join(__dirname, `silvia.html`))
+//     console.log('silvia.html')
+//   } else if (value === 'sampson') {
+//     res.sendFile(Path.join(__dirname, `sampson.html`))
+//     console.log('Sampson.html')
+//   }
+// })
+
+// 4. responf based on url parameter
+server.get('/profiles/:id', (req, res) => {
+  const value = req.params.id
   // const name = req.query.name
-  if (value === 'silvia') {
+  if (value === '1') {
     res.sendFile(Path.join(__dirname, `silvia.html`))
     console.log('silvia.html')
-  } else if (value === 'sampson') {
+  } else if (value === '2') {
     res.sendFile(Path.join(__dirname, `sampson.html`))
     console.log('Sampson.html')
   }
 })
-
 export default server
