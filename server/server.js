a4d697543d1f0cdb65117a3875586679c528e6f4
Cong <53280539+Jero327@users.noreply.github.com>
2023-10-16 11:10:23 +1300

    Resond with a string

--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ import * as URL from 'node:url'
 
 import express from 'express'
 
-
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
 const server = express()
 
 export default server
+
+server.get('/compliment', (req, res) => {
+  res.send(`<h1>You look amonzing today!</h1>`)
+})
