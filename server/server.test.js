6e4325b1aca1b4211f1bb795ed4806fea5b5d5a5
Vitor <142807308+vitor-fernandess@users.noreply.github.com>
2023-10-16 16:46:28 +1300

    adding first test

--- server/server.test.js
+++ server/server.test.js
@@ -1,10 +1,10 @@
 import request from 'supertest'
-import { server } from '.server.js'
+import server from './server.js'
 import { test, expect } from 'vitest'
 
 // GET '/' route responds with index.html text
 
 test("GET '/' res with index.html", async () => {
-  const response = await request(server).get('/')
-  expect(response.text).tomMatch('Damnnnn shortyyy!!')
+  const response = await request(server).get('/compliment')
+  expect(response.text).toContain('Damnnnn shortyyy!!')
 })
