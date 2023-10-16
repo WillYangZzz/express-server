94a0bc344c4ec63d1c28186013c4392dba023a75
Cong <53280539+Jero327@users.noreply.github.com>
2023-10-16 14:54:45 +1300

    5. Enable a folder of static files

--- server/server.js
+++ server/server.js
@@ -10,37 +10,35 @@ const server = express()
 
 server.use(express.static(Path.join(__dirname, 'public')))
 
-server.get('/compliment', (req, res) => {
-  res.send(`<h1>You look amonzing today!</h1>`)
+server.get('/', (req, res) => {
+  res.send(`<h1>Home Page</h1>`)
 })
 
-server.get('/profile', (req, res) => {
-  const fileName = req.query.name + '.html'
-  res.sendFile(Path.join(__dirname, fileName))
+server.get('/compliment', (req, res) => {
+  res.send(`<h1>You look amazing today!</h1>`)
 })
 
 server.get('/profiles/:id', (req, res) => {
   let fileName
+  console.log(req.params.id)
 
   switch (req.params.id) {
-    case '1':
-      fileName = 'silvia.html'
+    case '1': {
+      res.sendFile(Path.join(__dirname, 'public/silvia.html'))
       break
-    case '2':
-      fileName = 'sampson.html'
+    }
+    case '2': {
+      res.sendFile(Path.join(__dirname, 'public/sampson.html'))
       break
-    default:
-      res.send('Not found')
+    }
+    default: {
+      res.send('invalid page')
+    }
   }
-
-  res.sendFile(Path.join(__dirname, fileName))
-  console.log(Path.join(__dirname, fileName))
-  console.log(fileName)
-  console.log(req.params.id)
 })
-
-server.get('/customer', (req, res) => {
-  res.sendFile(Path.join(__dirname, 'public', 'silvia.html'))
+server.get('/profile', (req, res) => {
+  const fileName = req.query.name + '.html'
+  res.sendFile(Path.join(__dirname, 'public', fileName))
 })
 
 export default server
