6c52d0edb55809892af15ac500cfbe8e165af15b
Dan <143304904+danial-shahali@users.noreply.github.com>
2023-10-16 16:39:14 +1300

    added the challenge 7

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import * as Path from 'node:path/posix'
 import * as URL from 'node:url'
+//import Path from 'node:path'
 
 import express from 'express'
 
@@ -8,7 +9,9 @@ const __dirname = Path.dirname(__filename)
 
 const server = express()
 
-server.use(express.static(Path.join(__dirname, 'public')))
+server.use(express.static(Path.join(__dirname, '../public')))
+
+server.use(express.urlencoded({ extended: false }))
 
 server.get('/compliment', (req, res) => {
   res.send('<h1>"We are awesome"</h1>')
@@ -33,5 +36,29 @@ server.get('/profile', (req, res) => {
   const name = req.query.name
   res.sendFile(Path.join(__dirname, `../public/${name}.html`))
 })
+
+server.get('/profiles/:id', (req, res) => {
+  const value = req.params.id
+  if (value === '1') {
+    res.sendFile(Path.join(__dirname, '../public/silvia.html'))
+  } else if (value === '2') {
+    res.sendFile(Path.join(__dirname, '../public/sampson.html'))
+  }
+})
+
+server.get('/get-name', (req, res) => {
+  res.sendFile(Path.join(__dirname, '../public/get-name.html'))
+})
+
+server.post('/named-compliment', (req, res) => {
+  //console.log(req.body)
+  const value = req.body.customerName
+  res.send(`<h1>Good Job ${value}!`)
+  //res.redirect('/')
+})
+
+// server.get('/named-compliment', (req, res) => {
+//   res.send('<h1>"We are truley awesome"</h1>')
+// })
 
 export default server
