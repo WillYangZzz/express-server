e766b370d707b963162be6c8bb41d09781314248
Ricky <65267289+ricky-huang-dev@users.noreply.github.com>
2023-10-16 15:37:06 +1300

    refactor code

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 import * as Path from 'node:path/posix'
 import * as URL from 'node:url'
-
 import express from 'express'
 
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
 const server = express()
-
 export default server
 
 server.get('/compliment', (req, res) => {
@@ -16,16 +14,13 @@ server.get('/compliment', (req, res) => {
 
 server.use(express.static(Path.join(__dirname, './public')))
 
-const silviaPath = Path.join(__dirname, './public/silvia.html')
-const sampsonPath = Path.join(__dirname, './public/sampson.html')
+const silviaPath = Path.join(__dirname, 'public/silvia.html')
+const sampsonPath = Path.join(__dirname, 'public/sampson.html')
 
 // query example
 server.get('/profile', (req, res) => {
   // res.sendFile(sampsonPath)
   let name = req.query.name
-
-  console.log(req.query.name)
-
   if (name == 'silvia') {
     res.sendFile(silviaPath)
   } else if (name == 'sampson') {
@@ -37,9 +32,6 @@ server.get('/profile', (req, res) => {
 server.get('/profiles/:s', (req, res) => {
   // res.sendFile(sampsonPath)
   let name = req.params.s
-
-  console.log(req.params.s)
-
   if (name == '1') {
     res.sendFile(silviaPath)
   } else if (name == '2') {
