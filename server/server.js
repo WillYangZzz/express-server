6051305ec74bfccdc1b7a9e1ae73f3c7d7575d3e
Pathik <142643540+pathik-modi@users.noreply.github.com>
2023-10-16 16:24:09 +1300

    Working form with compliment message with name

--- server/server.js
+++ server/server.js
@@ -8,13 +8,18 @@ const __dirname = Path.dirname(__filename)
 
 const server = express()
 
+// creating public statiuc folder
 server.use(express.static(Path.join(__dirname, 'public')))
 
+// express can handle POST requests with forms
+server.use(express.urlencoded({ extended: false }))
+
+// 1. simple page with html
 server.get('/compliment', (req, res) => {
   res.send('<h1>Damnnnn shortyyy!!</h1>')
 })
 
-// // 3. Respond based on the query
+// 3. Respond based on the query
 server.get('/profile', (req, res) => {
   const profile = req.query.profile
   if (profile === 'silvia') {
@@ -30,10 +35,19 @@ server.get('/profiles/:id', (req, res) => {
   // const name = req.query.name
   if (value === '1') {
     res.sendFile(Path.join(__dirname, `silvia.html`))
-    console.log('silvia.html')
   } else if (value === '2') {
     res.sendFile(Path.join(__dirname, `sampson.html`))
-    console.log('Sampson.html')
   }
 })
+
+// 7. form input and output with a compliment
+
+server.get('/get-name', (req, res) => {
+  res.sendFile(Path.join(__dirname, `/public/get-name.html`))
+})
+
+server.post('/get-name', (req, res) => {
+  res.send(`<h2>Thanks ${req.body.getName} ya legend!</h2>`)
+})
+
 export default server
