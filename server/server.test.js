222b00c6ce244e0add2cf3f197c8a3f481416e8c
Will <105895841+WillYangZzz@users.noreply.github.com>
2023-10-16 17:14:09 +1300

    add test for profiles

--- server/server.test.js
+++ server/server.test.js
@@ -7,3 +7,8 @@ test("GET '/' res with compliment.html", async () => {
   const response = await request(server).get('/')
   expect(response.text).toContain('handsome')
 })
+
+test("GET '/profiles/1' res with value", async () => {
+  const response = await request(server).get('/profiles/2')
+  expect(response.text).toContain('sampson profile')
+})
