ae88ddd20f42c332ed9470d3c67b62a393b4e475
Logan <140151668+logan-bruce01@users.noreply.github.com>
2023-10-16 13:46:28 +1300

    completed tasks

--- server/server.js
+++ server/server.js
@@ -3,10 +3,33 @@ import * as URL from 'node:url'
 
 import express from 'express'
 
-
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
 const server = express()
+
+server.use(express.static(Path.join(__dirname, `/public`)))
+
+// server.get('/', (res, req) => {
+//   res.send(`You're the man!`)
+// })
+
+server.get('/compliment', (req, res) => {
+  res.send('Your the Man!')
+})
+
+server.get('/profile/', (req, res) => {
+  const name = req.query.name
+
+  console.log(name)
+  res.sendFile(Path.join(__dirname, `/public/${name}.html`))
+})
+
+server.get('/profile/:name', (req, res) => {
+  const name = req.params.name
+
+  console.log(name)
+  res.sendFile(Path.join(__dirname, `/public/${name}.html`))
+})
 
 export default server
