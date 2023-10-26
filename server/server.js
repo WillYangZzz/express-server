113cc06bb9d7e0553f4879f93c5c26ec1348a19e
xavier-kirkpatrick <kirkpatrickxavier@gmail.com>
2023-10-27 08:18:54 +1300

    added routes

--- server/server.js
+++ server/server.js
@@ -3,10 +3,22 @@ import * as URL from 'node:url'
 
 import express from 'express'
 
-
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
 const server = express()
 
+server.get('/compliment', (req, res) => {
+  res.send("You're the best!")
+})
+
+server.get('/fortune', (req, res) => {
+  res.sendFile(Path.join(__dirname, 'silvia.html'))
+})
+
 export default server
+
+// Remember:
+// Public folder for HTML and CSS files
+// Public folder for images
+// Expose public folder in _dirname^^
