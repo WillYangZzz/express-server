e7851600db3e3341c2b5c974425bd0ae3bb773e8
Will Yang <willyangzzz@gmail.com>
2023-12-13 10:16:41 +1300

    create a compliment route

--- server/server.js
+++ server/server.js
@@ -3,10 +3,22 @@ import * as URL from 'node:url'
 
 import express from 'express'
 
-
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
-
 const server = express()
+
+// express will now expose the public/ folder to be accessible for any web requests
+server.use(express.static(Path.join(__dirname, 'public')))
+// express can handle POST requests with forms
+server.use(express.urlencoded({ extended: false }))
+
+// this is the homepage and users can view it by typing localhost:3000/
+server.get('/', (req, res) => {
+  res.sendFile(Path.join(__dirname, 'index.html'))
+})
+
+server.get('/compliment', (req, res) => {
+  res.sendFile(Path.join(__dirname, 'index.html'))
+})
 
 export default server
