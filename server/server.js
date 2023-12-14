3f10b0d591839eb1e4836f06eb626812b0c0900f
Will Yang <willyangzzz@gmail.com>
2023-12-15 11:26:58 +1300

    add compliment page using req.params and req.query

--- server/server.js
+++ server/server.js
@@ -21,14 +21,38 @@ server.get('/compliment', (req, res) => {
   res.send(`<h1>You look amazing today!<h1>`)
 })
 
-server.get('/named-compliment', (req, res) => {
-  res.sendFile(Path.join(__dirname, 'public', 'get-name.html'))
+server.get('/compliment/:name', (req, res) => {
+  const name = req.params.name
+  const person = req.query.person
+  res.send(`<h1>Have a good day ${name} and ${person}</h1>`)
 })
 
-server.post('/named-compliment', (req, res) => {
-  res.send(
-    `${req.body.name}, you look great today! </br></br><a href='/named-compliment'>Go back</a>`
-  )
+server.get('/profile/', (req, res) => {
+  const name = req.query.name
+  console.log(name)
+  res.sendFile(__dirname, `../public/${name}.html`)
 })
+
+server.get('/profile/:name', (req, res) => {
+  const name = req.query.name
+
+  if (name === '1') {
+    res.sendFile(Path.join(__dirname, `public/silvia.html`))
+  } else if (name === '2') {
+    res.sendFile(Path.join(__dirname, `public/sampson.html`))
+  } else {
+    res.sendFile(Path.join(__dirname, `public/${name}.html`))
+  }
+})
+
+// server.get('/named-compliment', (req, res) => {
+//   res.sendFile(Path.join(__dirname, 'public', 'get-name.html'))
+// })
+
+// server.post('/named-compliment', (req, res) => {
+//   res.send(
+//     `${req.body.name}, you look great today! </br></br><a href='/named-compliment'>Go back</a>`
+//   )
+// })
 
 export default server
